d6a1070b982905ad233ec2678d8afb44158b1e50
khalifa005 <mahmudkhalifa12@outlook.com>
2020-03-17 11:26:01 +0200

    improved code quality

--- js/dragCv.js
+++ js/dragCv.js
@@ -2,41 +2,37 @@ console.log("test visablity");
 dragElement(document.getElementById("drag"));
 
 function dragElement(idDoc) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
   if (document.getElementById(idDoc.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
     document.getElementById(idDoc.id + "header").onmousedown = dragMouseDown;
   } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
     idDoc.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     idDoc.style.top = (idDoc.offsetTop - pos2) + "px";
     idDoc.style.left = (idDoc.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
   }
