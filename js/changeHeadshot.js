546d4c0e629db7b57ef8221c9826b73db035d05b
khalifa005 <mahmudkhalifa12@outlook.com>
2020-03-17 21:47:15 +0200

    animations added

---
+++ js/changeHeadshot.js
@@ -0,0 +1,7 @@
+function hover(element) {
+  element.setAttribute('src', 'http://www.vitorazevedo.net/external_files/loading_small.png');
+}
+
+function unhover(element) {
+  element.setAttribute('src', './imgs/logo.png')
+}
